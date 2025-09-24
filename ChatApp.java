55380f688d9f779a976264510802e1fc2754c314
Kamalesh Ashokkumar Kowsalya <kk1676332@gmail.com>
2025-09-24 16:24:58 +0300

    Add files via upload

---
+++ ChatApp.java
@@ -0,0 +1,88 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class ChatApp extends JFrame implements Runnable,ActionListener {
+    JTextField textField;
+    JTextArea textArea;
+    JButton send;
+
+    ServerSocket serverSocket;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    Thread chat;
+    ChatApp(){
+        textField = new JTextField();
+        textField.setBounds(50,50,100,30);
+
+        textArea = new JTextArea();
+        textArea.setBounds(200,50,180,120);
+
+        send = new JButton("Send");
+        send.setBounds(200,230,70,40);
+        send.addActionListener(this);
+
+        try {
+            serverSocket = new ServerSocket(12000);
+            socket = serverSocket.accept();
+
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+
+        }
+
+        add(textField);
+        add(textArea);
+        add(send);
+
+        chat = new Thread(this);
+        chat.setDaemon(true);
+        chat.start();
+
+
+        setTitle("Chat App");
+        setSize(500,500);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ChatApp();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String msg = textField.getText();
+        textArea.append("ChatApp:"+msg+"\n");
+        textField.setText("");
+
+        try {
+            dataOutputStream.writeUTF(msg);
+            dataOutputStream.flush();
+        } catch (IOException ex) {
+
+        }
+
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try {
+                String msg = dataInputStream.readUTF();
+                textArea.append("New App:"+msg+"\n");
+            } catch (IOException e) {
+
+            }
+        }
+    }
+}
